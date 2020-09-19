3d09c21a2e41698026ead22a19b6ef9688e857b0
Justin Yu <justinyu1618@gmail.com>
2020-09-19 12:47:55 -0400

    minor changes

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 
 DEBUG=True
-
 base = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(base, 'app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
